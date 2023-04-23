f1a68c965ae982d1dc27aa83ec4212f48a8db959
lakshmi12priya <121346748+lakshmi12priya@users.noreply.github.com>
2023-04-23 18:42:23 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+$conn=mysqli_connect('localhost','laks','No','sel_project');
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <?php include 'header.php'; ?>
+        <div class="provision">
+            <!-- <a href = "./timetable/index_tt.php"><button class = 'btnn'>Time Table Scheduling</button></a> -->
+            <a href = "./hall_allocation/index_hall.php"><button class = 'btnn'>Hall Allocation</button></a>
+            <a href = "./admin_view_and_edit/form0.php"><button class = 'btnn'>Admin view and edit</button></a>
+            <a href = "./seating/get_session.php"><button class = 'btnn'>Seating Arrangement</button></a>
+            <a href = "./assign_faculty/dept.php"><button class = 'btnn'>Assign Faculty</button></a>
+            <a href = "./qp/index_qp.php"><button class = 'btnn'>Question paper distribution</button></a>
+            <a href = "./anspaper/index_ans.php"><button class = 'btnn'>Answer paper distribution</button></a>
+        </div>
+    </body>
+</html>
+
