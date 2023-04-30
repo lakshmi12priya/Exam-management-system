417707d403ded24e2803671633e331d8f2d077e5
lakshmi12priya <121346748+lakshmi12priya@users.noreply.github.com>
2023-04-30 19:09:45 +0530

    Update display.php

--- hall_allocation/display.php
+++ hall_allocation/display.php
@@ -1,6 +1,5 @@
 <?php
-    include 'header.php';
-    include 'connection.php';
+    include '../header.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,14 +7,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-    
+    <title>Document</title>  
+    <link rel="stylesheet" href="../style.css">  
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.js">
     </script>
 </head>
-<div class="makepdf">
-<table>
+<body>
+<div id="makepdf">
+    <br><br>
+    <?php $sql="SELECT dept_name from u_dept where dept_id='$_SESSION[dept_id]'";
+    $query=mysqli_query($conn,$sql); 
+    $dept=mysqli_fetch_assoc($query);?>
+    <h2 style="text-align:center;">Department: <?php echo "$dept[dept_name]"; ?></h2> 
+    <h2 style="text-align:center;">Session: <?php echo "$_SESSION[chosen_session]"; ?></h2>
+    <br><br>
+    <table>
     <tr>
         <th>Date</th>
         <th>Subject</th>
@@ -23,20 +29,17 @@
         <th>Faculty</th>
     </tr>
 <?php
-    $dets_query='Select ex_date,t.ex_session,t.course_code,course_name
-           from exam_timetable t,u_course
-           where t.course_code=u_course.course_code';
+    $dets_query="Select ex_date,t.ex_session,t.course_code,course_name,room_num,invigilator_id,no_of_students
+           from exam_ROOMS t,u_course
+           where t.course_code=u_course.course_code and u_course.dept_id='$_SESSION[dept_id]'";
     $dets_run=mysqli_query($conn,$dets_query);
     while(($details=mysqli_fetch_assoc($dets_run)))
     {
         echo "<tr>
         <td>$details[ex_date]</td>
-        <td>$details[course_name] </td>";
-        $room=$_POST['room'];
-        $faculty=$_POST['faculty'];
-        echo "
-        <td>$room</td>
-        <td>$faculty</td>
+        <td>$details[course_name] </td>
+        <td>$details[room_num]</td>
+        <td>$details[invigilator_id]</td>
         </tr>";
     }
 ?>
@@ -52,4 +55,8 @@
                     html2pdf().from(makepdf).save();
                 });
             </script> 
+    <div class="submit_dets">
+        <button  class="submit_hall"><a href="allocate_final.php">Back</a></button>
+    </div>
+</body>
 </html>
